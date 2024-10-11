878933ac426e9bcf35442216d63eb913e2ba3ae2
matthewmonolith <matthew.page2016@outlook.com>
2024-10-11 19:06:03 +0100

    reviews page wip

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,7 +1,30 @@
-function ReviewsPage() {
+import { deleteReviewAction, fetchProductReviewsByUser } from "@/utils/actions";
+import ReviewCard from "@/components/reviews/ReviewCard";
+import SectionTitle from "@/components/global/SectionTitle";
+import FormContainer from "@/components/form/FormContainer";
+import { IconButton } from "@/components/form/Buttons";
+
+async function ReviewsPage() {
+  const reviews = await fetchProductReviewsByUser();
+  if (reviews.length === 0) {
+    return <SectionTitle text="You have no reviews yet" />;
+  }
+
   return (
-    <div>ReviewsPage</div>
-  )
+    <>
+      <SectionTitle text="Your reviews" />
+      <section className="grid md:grid-cols-2 gap-8 mt-4">
+        {reviews.map((review) => {
+          const {comment, rating} = review
+          const {name, image} = review.product
+          const reviewInfo = {
+            comment, rating, name, image
+          }
+          return <ReviewCard key={review.id} reviewInfo={reviewInfo}></ReviewCard>
+        })}
+      </section>
+    </>
+  );
 }
 
-export default ReviewsPage
+export default ReviewsPage;
--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,53 +12,62 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { links } from "@/utils/links";
 import { UserIcon } from "lucide-react";
-import { SignedOut, SignedIn, SignInButton, SignUpButton } from "@clerk/nextjs";
+import {
+  SignedOut,
+  SignedIn,
+  SignInButton,
+  SignUpButton,
+  ClerkProvider,
+} from "@clerk/nextjs";
 import SignOutLink from "./SignOutLink";
-import { auth } from "@clerk/nextjs/server";
+import { useAuth } from "@clerk/nextjs";
 
-async function LinksDropdown() {
-  const { userId } = await auth();
-  const isAdmin = userId === process.env.ADMIN_USER_ID;
+function LinksDropdown() {
+  const { userId } = useAuth();
+  const isAdmin = userId === process.env.NEXT_PUBLIC_ADMIN_USER_ID;
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex gap-4 max-w-[100px]">
-          <LuAlignLeft className="w-6 h-6" />
-          <UserIcon />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
-        <SignedOut>
-          <DropdownMenuItem>
-            <SignInButton mode="modal">
-              <button className="w-full text-left">Login</button>
-            </SignInButton>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <SignUpButton mode="modal">
-              <button className="w-full text-left">Register</button>
-            </SignUpButton>
-          </DropdownMenuItem>
-        </SignedOut>
-        <SignedIn>
-          {links.map((link) => {
-            if (link.label === "dashboard" && !isAdmin) return null;
-            return (
-              <DropdownMenuItem key={link.href}>
-                <Link href={link.href} className="capitalize w-full">
-                  {link.label}
-                </Link>
-              </DropdownMenuItem>
-            );
-          })}
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <SignOutLink />
-          </DropdownMenuItem>
-        </SignedIn>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <ClerkProvider dynamic>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex gap-4 max-w-[100px]">
+            <LuAlignLeft className="w-6 h-6" />
+            <UserIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
+          <SignedOut>
+            <DropdownMenuItem>
+              <SignInButton mode="modal">
+                <button className="w-full text-left">Login</button>
+              </SignInButton>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <SignUpButton mode="modal">
+                <button className="w-full text-left">Register</button>
+              </SignUpButton>
+            </DropdownMenuItem>
+          </SignedOut>
+          <SignedIn>
+            {links.map((link) => {
+              if (link.label === "dashboard" && !isAdmin) return null;
+              return (
+                <DropdownMenuItem key={link.href}>
+                  <Link href={link.href} className="capitalize w-full">
+                    {link.label}
+                  </Link>
+                </DropdownMenuItem>
+              );
+            })}
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <SignOutLink />
+            </DropdownMenuItem>
+          </SignedIn>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </ClerkProvider>
   );
 }
 
