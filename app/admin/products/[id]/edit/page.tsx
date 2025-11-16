f86f94219c60e0a3c00274b64cd0deb171ab3d87
Matthew Page <matthew.page2016@outlook.com>
2025-11-16 13:21:45 +0000

    prop promise fix

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -11,12 +11,12 @@ import { SubmitButton } from "@/components/form/Buttons";
 import CheckboxInput from "@/components/form/CheckboxInput";
 import ImageInputContainer from "@/components/form/ImageInputContainer";
 
-// interface EditProductPageProps {
-//   params: { id: string };
-// }
+interface EditProductPageProps {
+  params: Promise<{ id: string }>;
+}
 
-async function EditProductPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+async function EditProductPage({ params }: EditProductPageProps) {
+  const { id } = await  params;
   const product = await fetchAdminProductDetails(id);
   const { name, company, description, featured, price, image } = product;
 
@@ -36,26 +36,13 @@ async function EditProductPage({ params }: { params: { id: string } }) {
         <FormContainer action={updateProductAction}>
           <div className="grid gap-4 md:grid-cols-2 my-4">
             <input type="hidden" name="id" value={id} />
-            <FormInput
-              type="text"
-              name="name"
-              label="product name"
-              defaultValue={name}
-            />
+            <FormInput type="text" name="name" label="product name" defaultValue={name} />
             <FormInput type="text" name="company" defaultValue={company} />
             <PriceInput defaultValue={price} />
           </div>
-          <TextAreaInput
-            name="description"
-            labelText="product description"
-            defaultValue={description}
-          />
+          <TextAreaInput name="description" labelText="product description" defaultValue={description} />
           <div className="mt-6">
-            <CheckboxInput
-              name="featured"
-              label="featured"
-              defaultChecked={featured}
-            />
+            <CheckboxInput name="featured" label="featured" defaultChecked={featured} />
           </div>
           <SubmitButton text="update product" className="mt-8" />
         </FormContainer>
